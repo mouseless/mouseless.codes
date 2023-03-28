1e63b7e0efd9946cd5db90bb50b92d73b069a4fd
Sefer Mirza <sefermirza@gmail.com>
2023-03-28 16:56:22 +0300

    add trailing slash

--- .theme/pages/[...content-page].vue
+++ .theme/pages/[...content-page].vue
@@ -1,3 +1,21 @@
 <template>
-  <ContentDoc />
+  <ContentDoc v-if="!trailingSlash" />
 </template>
+<script setup>
+import { useRoute, onMounted } from "#imports";
+
+const route = useRoute();
+const trailingSlash = route.path !== "/" && route.path.endsWith("/");
+
+onMounted(async () => {
+  if(trailingSlash) {
+    const { path, query, hash } = route;
+    const nextPath = path.replace(/\/+$/, "");
+    const nextRoute = { path: nextPath, query, hash };
+
+    // works only if `router.options.strict` is enabled in `nuxt.config.ts`
+    // replace prevents browser to record this navigation in its history
+    await navigateTo(nextRoute, { replace: true });
+  }
+});
+</script>
