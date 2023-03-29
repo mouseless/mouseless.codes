d2cf687e1965408171f81fda2a1c2c8d693064f0
Sefer Mirza <sefermirza@gmail.com>
2023-03-29 18:06:24 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,13 +2,15 @@
 :BEGIN
 echo (1) Dev
 echo (2) Dev with build
-echo (3) Local
-echo (4) Eslint
-echo (5) Eslint Fix
-CHOICE /N /C:12345 /M "Pick mode"
-IF ERRORLEVEL == 5 GOTO Eslint-Fix
-IF ERRORLEVEL == 4 GOTO Eslint
-IF ERRORLEVEL == 3 GOTO Local
+echo (3) Local with generate
+echo (4) Local
+echo (5) Eslint
+echo (6) Eslint Fix
+CHOICE /N /C:123456 /M "Pick mode"
+IF ERRORLEVEL == 6 GOTO Eslint-Fix
+IF ERRORLEVEL == 5 GOTO Eslint
+IF ERRORLEVEL == 4 GOTO Local
+IF ERRORLEVEL == 3 GOTO Local-with-generate
 IF ERRORLEVEL == 2 GOTO Dev-Build
 IF ERRORLEVEL == 1 GOTO Dev
 GOTO END
@@ -21,6 +23,10 @@ cd .theme
 npm run dev:build
 cd ..
 :Local
+cd .theme
+npx http-server .theme/.output/public -p 3000
+cd ..
+:Local-with-generate
 cd .theme
 npm run local
 cd ..
