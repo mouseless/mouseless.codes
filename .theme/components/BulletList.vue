f1547f2a2c855336bee5f349c4bc719ac4900b0b
Sefer Mirza <sefermirza@gmail.com>
2024-01-16 19:41:56 +0300

    add bullet list component

---
+++ .theme/components/BulletList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bullet-list">
+    <ContentSlot :use="$slots.default" />
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  bullet: {
+    type: String,
+    default: null
+  }
+});
+
+const bulletUrl = ref(props.bullet);
+</script>
+<style scoped lang="scss">
+li {
+  list-style-image: url(v-bind(bulletUrl));
+  padding-inline-start: 1ch;
+}
+</style>
--- demo.md
+++ demo.md
@@ -9,3 +9,13 @@ img: https://mouseless.github.io/brand/assets/logo/svg/logo-mark-primary.svg
 ---
 Lorem ipsum dolor sit amet
 ::
+
+## Bullet List Component
+
+::bullet-list
+---
+bullet: https://pics.freeicons.io/uploads/icons/png/16741343401556273568-512.png
+---
+- customer1
+- customer2
+::
