dec81d934b08d5bb6b36becbe4ecd1df0625d920
cihandeniz <cihan@gcbrains.com>
2024-02-08 17:33:54 +0300

    refactor button css to fit bem methodology

--- .theme/components/LinkButton.vue
+++ .theme/components/LinkButton.vue
@@ -1,12 +1,11 @@
 <template>
-  <div
+  <NuxtLink
     class="link-button"
     :class="`link-button--type_${type}`"
+    :to="to"
   >
-    <NuxtLink class="link-button__link" :to="to">
-      {{ text }}
-    </NuxtLink>
-  </div>
+    {{ text }}
+  </NuxtLink>
 </template>
 <script setup>
 defineProps({
@@ -31,57 +30,24 @@ defineProps({
   justify-content: center;
   padding-inline: 18px;
   padding-block: 9px;
+  text-decoration: none;
+  border-radius: var(--border-radius);
 
-  &--type_cta {
-    background-color: var(--color-logo-mark);
-    padding-inline: 30px;
-    padding-block: 10px;
-
-    &:hover {
-      background-color: var(--color-fg-box);
-
-      .link-button__link {
-        color: var(--color-fg-mute);
-      }
-    }
+  &:hover {
+    background-color: var(--color-bg-mute);
+    color: var(--color-fg);
   }
 
   &--type_default {
     background: var(--color-fg-mute);
-    border-radius: var(--border-radius);
-
-    .link-button__link {
-      color: var(--color-bg-mute);
-    }
-
-    &:hover {
-      background: var(--color-bg-mute);
-
-      .link-button__link {
-        color: var(--color-fg-mute);
-      }
-    }
-
-    &-bg {
-      background: var(--color-bg-mute);
-      border-radius: var(--border-radius);
-
-      &:hover {
-        background: var(--color-fg-mute);
-
-        .link-button__link {
-          color: var(--color-bg-mute);
-        }
-      }
-
-      .link-button__link {
-        color: var(--color-fg-mute);
-      }
-    }
+    color: var(--color-bg-mute);
   }
 
-  &__link {
-    text-decoration: none;
+  &--type_cta {
+    background-color: var(--color-logo-mark);
+    color: var(--color-white-lightest);
+    padding-inline: 30px;
+    padding-block: 10px;
   }
 }
 </style>
