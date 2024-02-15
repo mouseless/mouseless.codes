65b1e24c67149633c580550140714ee19d81a10f
Sefer Mirza <sefermirza@gmail.com>
2024-02-15 19:39:23 +0300

    rename and update component

---
+++ .theme/components/GoogleForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="frame">
+    <iframe
+      :src="source"
+      class="frame__form"
+    >
+      <slot />
+    </iframe>
+    <div v-if="meeting" class="frame__schedule">
+      <button>
+        schedule meeting
+      </button>
+    </div>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  source: {
+    type: String,
+    default: ""
+  },
+  height: {
+    type: String,
+    default: ""
+  },
+  meeting: {
+    type: Boolean,
+    default: true
+  }
+});
+
+const height = props.height || "auto";
+</script>
+<style lang="scss" scoped>
+.frame {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &__form {
+    width: 100%;
+    height: v-bind(height);
+    border: 0;
+  }
+
+  &__schedule {
+    width: 200px;
+  }
+}
+</style>
--- demo.md
+++ demo.md
@@ -114,7 +114,14 @@ Maxime mollitia, molestiae quas vel sint commodi repudiandae
 
 ## Form
 
-:form
+:::google-form
+---
+source: "https://docs.google.com/forms/d/e/1FAIpQLScTupsQqnyg6-SJgZLY_RG6dkzhl0I84FE_CC0DlxxFLJ8WpA/viewform?embedded=true"
+height: "950px"
+meeting: false
+---
+Loading...
+:::
 
 ## PR List
 
