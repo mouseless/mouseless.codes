af5e5fec8159e46a8a8d17005ad17b8f340a2836
Sefer Mirza <sefermirza@gmail.com>
2024-02-20 19:07:42 +0300

    update inlineframe porps

--- .theme/components/InlineFrame.vue
+++ .theme/components/InlineFrame.vue
@@ -1,30 +1,42 @@
 <template>
   <iframe
-    :src="source"
     class="frame"
     scrolling="no"
     :height="height"
+    :src="source"
+    :width="width"
   >
     <slot />
   </iframe>
 </template>
 <script setup>
 const props = defineProps({
+  border: {
+    type: String,
+    default: null
+  },
   height: {
     type: String,
-    default: ""
+    default: null
   },
   source: {
     type: String,
     default: ""
+  },
+  width: {
+    type: String,
+    default: null
   }
 });
+
+const border = ref(props.border || "1px");
 const height = ref(props.height || "600px");
+const width = ref(props.width || "100%");
 </script>
 <style lang="scss" scoped>
 .frame {
-  width: 100%;
-  border: 0;
   border-radius: var(--border-radius);
+  border-style: solid;
+  border-width: v-bind(border);
 }
 </style>
