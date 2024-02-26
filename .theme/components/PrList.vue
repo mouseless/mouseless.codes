ea39bd229adc3453f6dd6d05d7e3df79a43d158e
Sefer Mirza <sefermirza@gmail.com>
2024-02-26 16:08:28 +0300

    move switcher to own file

--- .theme/components/PrList.vue
+++ .theme/components/PrList.vue
@@ -16,16 +16,7 @@
             </button>
           </li>
         </ul>
-        <div
-          class="switcher repo-list__switcher"
-          :class="`switcher--status_${prState}`"
-          @click="switcher"
-        >
-          <button class="switcher__btn" />
-          <div class="switcher__text">
-            {{ prState === "all" ? "All" : "Open" }}
-          </div>
-        </div>
+        <Switcher :action="switcher" :status="prState" />
       </div>
     </div>
     <div class="pr-list__prs">
@@ -189,57 +180,6 @@ function getState(object) {
     &--active {
       margin-left: 1ch;
     }
-  }
-}
-
-.switcher {
-  display: flex;
-  align-items: center;
-  border-radius: var(--border-radius);
-  color: var(--color-fg-mute);
-  cursor: pointer;
-  font-weight: bold;
-  font-size: smaller;
-  height: 25px;
-  position: relative;
-  width: 63px;
-  margin: 0 auto;
-
-  &--status {
-    &_open {
-      background-color: hsl(from var(--color-green) h s calc(l - .2));
-      color: var(--color-white);
-      justify-content: start;
-
-      .switcher__btn {
-        right: 2px;
-      }
-    }
-
-    &_all {
-      background-color: var(--color-gray);
-      justify-content: end;
-
-      .switcher__btn {
-        left: 2px;
-      }
-    }
-  }
-
-  &__text {
-    margin-left: 7px;
-    margin-right: 8px;
-  }
-
-  &__btn {
-    position: absolute;
-    width: 21px;
-    height: 21px;
-    background-color: var(--color-white);
-    border-radius: 50%;
-    border: 0;
-    cursor: pointer;
-    top: 2px;
   }
 }
 
